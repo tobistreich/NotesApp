e1aa1b0587ec6701f2e5eb495b78611d7138ad0e
tobiasstreich <tobias.streich@icloud.com>
2023-09-22 06:48:35 +0200

    project init and add

---
+++ NotesApp/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NotesApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string input_txt = "";
+        string title_txt = "";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void create_note(object sender, RoutedEventArgs e)
+        {
+            input_txt = input.Text;
+            title_txt = title.Text;
+
+            System.Windows.Controls.Button newBtn = new Button();
+
+            newBtn.Content = title_txt;
+            newBtn.Name = title_txt;
+           
+
+        }
+    }
+}
